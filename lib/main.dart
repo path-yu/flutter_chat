9e8d48eff980e5f034d93ecd69e69e52da837bb3
path-yu <1974675011@qq.com>
2022-12-16 00:53:04 +0800

    fix: change

--- lib/main.dart
+++ lib/main.dart
@@ -62,7 +62,7 @@ class MyApp extends StatelessWidget {
               navigatorKey: navigatorKey, // set property
               debugShowCheckedModeBanner: false,
               routes: baseRoutes,
-              initialRoute: '/editUser',
+              initialRoute: '/',
               builder: EasyLoading.init(),
               theme: ThemeData(
                 // is not restarted.
